a412d552b9a72ffde1a906d65ed4d9643afb910f
fedorrychkov <fedorrychkov@yandex.ru>
2020-01-24 03:28:39 +0300

    Add constants

---
+++ src/constants/colors.js
@@ -0,0 +1,5 @@
+// @flow
+
+export const BLACK_COLOR = '#455A64';
+export const BLUE_COLOR = '#1E88E5';
+export const GRAY_COLOR = '#99ABB4';
---
+++ src/constants/types.js
@@ -0,0 +1,20 @@
+// @flow
+
+export const valueTypes = [
+  {
+    name: 'Name',
+    value: 'name'
+  },
+  {
+    name: 'Surname',
+    value: 'surname'
+  },
+  {
+    name: 'Phone',
+    value: 'phone'
+  },
+  {
+    name: 'Email',
+    value: 'email'
+  }
+];
